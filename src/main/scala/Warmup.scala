36d68c1a8ff9bfe720abbe02409827d8289988d6
kroker42 <git@frisch.se>
2022-08-17 15:02:49 +0100

    Fib tail

--- src/main/scala/Warmup.scala
+++ src/main/scala/Warmup.scala
@@ -1,9 +1,73 @@
+import scala.annotation.tailrec
+
 object Warmup {
 
+  def sum(begin: Int, end: Int) : Int = {
+    @tailrec
+    def sum_tail(acc:Int, begin:Int, end: Int) : Int = {
+      if (begin == end)
+        acc + end
+      else
+        sum_tail(acc + end, begin, end - 1)
+    }
+
+    if (begin > end)
+      sum_tail(0, end, begin)
+    else
+      sum_tail(0, begin, end)
+  }
   def sum_rec(begin: Int, end: Int) : Int = {
     if (begin == end)
       begin
     else
       end + sum_rec(begin, end - 1)
+  }
+
+  def concat(string: String, n: Int): String = {
+    @tailrec
+    def concat_tail(acc: String, n: Int): String = {
+      if (n == 1)
+        acc
+      else
+        concat_tail(string + acc, n - 1)
+    }
+
+    concat_tail(string, n)
+  }
+
+  def concat_rec(string: String, n: Int): String = {
+    if (n == 1)
+      string
+    else
+      string + concat(string, n - 1)
+  }
+
+  def fib(n: Int): Int = {
+    @tailrec def fib_tail(prev1: Int, prev2: Int, n: Int): Int = {
+      if (n == 3)
+        prev1 + prev2
+      else
+        fib_tail(prev2, prev1 + prev2, n - 1)
+    }
+
+    if (n < 3)
+      1
+    else
+      fib_tail(1, 1, n)
+  }
+
+  def isValid(expr:String) : Boolean = {
+    @tailrec def isValid_tail(count : Int, expr:String) : Boolean = {
+      if (expr == "")
+        count == 0
+      else {
+        expr.head match {
+          case '(' => isValid_tail(count + 1, expr.tail)
+          case ')' => isValid_tail(count - 1, expr.tail)
+          case _ => isValid_tail(count, expr.tail)
+        }
+      }
+    }
+    isValid_tail(0, expr)
   }
 }
--- src/test/scala/WarmupSuite.scala
+++ src/test/scala/WarmupSuite.scala
@@ -8,4 +8,46 @@ class WarmupSuite extends FunSuite {
     assertEquals(Warmup.sum_rec(-2, 4), 7)
   }
 
+  test("sum_tail returns the sum of the numbers between two numbers a and b") {
+    assertEquals(Warmup.sum(1, 1), 1)
+    assertEquals(Warmup.sum(1, 3), 6)
+    assertEquals(Warmup.sum(-2, 4), 7)
+    assertEquals(Warmup.sum(3, 1), 6)
+  }
+
+  def check_concat[T](
+                name: String,
+                underTest: (String, Int) => String
+              )(implicit loc: munit.Location): Unit = {
+    test(name) {
+      assertEquals(underTest("", 3), "")
+      assertEquals(underTest("foo", 1), "foo")
+      assertEquals(underTest("bah", 3), "bahbahbah")
+    }
+  }
+
+  check_concat("Tail recursive Concatenate a given string n times", Warmup.concat _)
+  check_concat("Recursive Concatenate a given string n times", Warmup.concat_rec _)
+
+  test("Calculate fibonacci number for a provided value n") {
+    assertEquals(Warmup.fib(1), 1)
+    assertEquals(Warmup.fib(2), 1)
+    assertEquals(Warmup.fib(3), 2)
+    assertEquals(Warmup.fib(4), 3)
+    assertEquals(Warmup.fib(7), 13)
+  }
+
+  test("Bracket syntax is valid") {
+    assert(Warmup.isValid(""))
+    assert(Warmup.isValid("()"))
+    assert(Warmup.isValid("(())"))
+    assert(Warmup.isValid("(())()"))
+    assert(Warmup.isValid("(()(()))()"))
+    assert(!Warmup.isValid(")("))
+    assert(!Warmup.isValid("(()("))
+    assert(!Warmup.isValid("(()))"))
+  }
+
+
+
 }
